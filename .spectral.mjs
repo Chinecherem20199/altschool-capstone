abb73c169c76be01f31892c2523e39265246e4d5
Chinecherem Augustina Ugwuanyi <chitina2018@gmail.com>
2023-06-30 21:35:35 +0100

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwODc1MA/spectral.js?branch_slug=main&token=0fc99765-2d03-485b-82ba-6fdfd8d99744";
+export default { extends: ruleset };
